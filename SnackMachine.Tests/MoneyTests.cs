8e1c8521bbd6717e26f275a3a16bb06e08a75d9a
ChesterFalmen <65627281+ChesterFalmen@users.noreply.github.com>
2022-11-08 22:00:10 +0200

    TA-01_YurchenkoVasyl

--- SnackMachine.Tests/MoneyTests.cs
+++ SnackMachine.Tests/MoneyTests.cs
@@ -5,10 +5,33 @@ namespace SnackMachine.Tests
 {
     public class MoneyTests
     {
-        [Fact]
-        public void Test1()
+        /*=================================TA-01_YurchenkoVasyl=============================*/
+        [Theory]
+        [InlineData(0,0,0,0,0,0.0)]
+        [InlineData(0, 1, 0, 0, 0, 0.0)]
+        [InlineData(0, 0, 1, 0, 0, 0.0)]
+        [InlineData(0, 0, 0, 1, 0, 0.0)]
+        [InlineData(0, 0, 0, 0, 1, 0.0)]
+        [InlineData(0, 0, 0, 0, 0, 1.0)]
+        public void operatorravno_returnravno(int oneCent, int tenCent,int quarter, int oneDollar, int fiveDollar, int twentyDollar)
         {
+            var sut_1 = new Money(oneCent, tenCent, quarter, oneDollar, fiveDollar, twentyDollar);
+            var sut_2 = new Money(oneCent, tenCent, quarter, oneDollar, fiveDollar, twentyDollar);
 
+            bool expected = true;
+
+            Assert.Equal(expected,sut_1 == sut_2);
         }
+        [Fact]
+        public void operatorneravno_returnneravno()
+        {
+            var sut_1 = new Money(0, 0, 0, 1, 0, 0);
+            var sut_2 = new Money(0, 0, 0, 0, 1, 0);
+
+            bool expected = true;
+
+            Assert.Equal(expected, sut_1 != sut_2);
+        }
+        /*==================================================================================*/
     }
 }

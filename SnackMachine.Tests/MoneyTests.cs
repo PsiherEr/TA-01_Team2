868383e210eb9af7d41cd05a120f1557f1062959
ChesterFalmen <65627281+ChesterFalmen@users.noreply.github.com>
2022-11-22 01:17:23 +0200

    UPD 22.11.2022

--- SnackMachine.Tests/MoneyTests.cs
+++ SnackMachine.Tests/MoneyTests.cs
@@ -6,21 +6,35 @@ namespace SnackMachine.Tests
     public class MoneyTests
     {
         /*=================================TA-01_YurchenkoVasyl=============================*/
-        [Theory]
-        [InlineData(0,0,0,0,0,0.0)]
-        [InlineData(0, 1, 0, 0, 0, 0.0)]
-        [InlineData(0, 0, 1, 0, 0, 0.0)]
-        [InlineData(0, 0, 0, 1, 0, 0.0)]
-        [InlineData(0, 0, 0, 0, 1, 0.0)]
-        [InlineData(0, 0, 0, 0, 0, 1.0)]
-        public void operatorravno_returnravno(int oneCent, int tenCent,int quarter, int oneDollar, int fiveDollar, int twentyDollar)
+        [Fact]
+        public void operatorravno_returnravno_1()
         {
-            var sut_1 = new Money(oneCent, tenCent, quarter, oneDollar, fiveDollar, twentyDollar);
-            var sut_2 = new Money(oneCent, tenCent, quarter, oneDollar, fiveDollar, twentyDollar);
+            var sut_1 = new Money(0, 1, 0, 0, 0, 0);
+            var sut_2 = new Money(0, 1, 0, 0, 0, 0);
 
             bool expected = true;
+            Assert.Equal(expected, sut_1 == sut_2);
 
-            Assert.Equal(expected,sut_1 == sut_2);
+        }
+        [Fact]
+        public void operatorravno_returnravno_2()
+        {
+            var sut_1 = new Money(0, 1, 0, 0, 0, 0);
+            var sut_2 = new Money(0, 0, 0, 0, 0, 0);
+
+            bool expected = false;
+            Assert.Equal(expected, sut_1 == sut_2);
+
+        }
+        [Fact]
+        public void operatorravno_returnravno_3()
+        {
+            var sut_1 = new Money(0, 0, 0, 0, 0, 0);
+            var sut_2 = new Money(0, 0, 0, 0, 0, 0);
+
+            bool expected = true;
+            Assert.Equal(expected, sut_1 == sut_2);
+
         }
         [Fact]
         public void operatorneravno_returnneravno()

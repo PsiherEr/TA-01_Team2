4cebee827d346c38cac209d26b78ae0873577c18
XXX69-Maksim2003-69XXX <114422623+XXX69-Maksim2003-69XXX@users.noreply.github.com>
2023-01-16 15:06:18 +0200

    Lab2 (remake)

--- SnackMachine.Tests/MoneyTests.cs
+++ SnackMachine.Tests/MoneyTests.cs
@@ -5,21 +5,33 @@ namespace SnackMachine.Tests
 {
     public class MoneyTests
     {
-        [Fact]
-        public void OperatorPlus()
+        [Theory]
+        [InlineData(1, 0, 0, 0, 0, 0)]
+        [InlineData(0, 1, 0, 0, 0, 0)]
+        [InlineData(0, 0, 1, 0, 0, 0)]
+        [InlineData(0, 0, 0, 1, 0, 0)]
+        [InlineData(0, 0, 0, 0, 1, 0)]
+        [InlineData(0, 0, 0, 0, 0, 1)]
+        public void OperatorPlus(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
         {
-            var sut_1 = new Money(0, 1, 0, 0, 0, 0);
-            var sut_2 = new Money(0, 0, 1, 0, 0, 0);
-            decimal expected = 0.35m;
+            var sut_1 = new Money(oneCentCount, tenCentCount, quarterCount, oneDollarCount, fiveDollarCount, twentyDollarCount);
+            var sut_2 = new Money(oneCentCount, tenCentCount, quarterCount, oneDollarCount, fiveDollarCount, twentyDollarCount);
+            decimal expected = 2*(oneCentCount * 0.01m + tenCentCount * 0.10m + quarterCount * 0.25m + oneDollarCount + fiveDollarCount * 5 + twentyDollarCount * 20);
             Assert.Equal(expected, (sut_1 + sut_2).Amount);
         }
 
-        [Fact]
-        public void OperatorMinus()
+        [Theory]
+        [InlineData(1, 0, 0, 0, 0, 0)]
+        [InlineData(0, 1, 0, 0, 0, 0)]
+        [InlineData(0, 0, 1, 0, 0, 0)]
+        [InlineData(0, 0, 0, 1, 0, 0)]
+        [InlineData(0, 0, 0, 0, 1, 0)]
+        [InlineData(0, 0, 0, 0, 0, 1)]
+        public void OperatorMinus(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
         {
-            var sut_1 = new Money(0, 0, 0, 0, 2, 0);
-            var sut_2 = new Money(0, 0, 0, 0, 1, 0);
-            decimal expected = 5m;
+            var sut_1 = new Money(oneCentCount, tenCentCount, quarterCount, oneDollarCount, fiveDollarCount, twentyDollarCount);
+            var sut_2 = new Money(oneCentCount, tenCentCount, quarterCount, oneDollarCount, fiveDollarCount, twentyDollarCount);
+            decimal expected = 0;
             Assert.Equal(expected, (sut_1 - sut_2).Amount);
         }
     }
